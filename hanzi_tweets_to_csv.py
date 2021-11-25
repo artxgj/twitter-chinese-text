7614116811a36a7da06bf8bb477a9c5b42a84307
artxgj <beijingbayou@gmail.com>
2021-11-25 03:43:06 -0800

    modified hanzi_tweets_to_csv.py

--- hanzi_tweets_to_csv.py
+++ hanzi_tweets_to_csv.py
@@ -1,21 +1,15 @@
 import argparse
 import csv
 import pathlib
-
-from hanzi_helpers import next_hanzi_tweet
-from twitter_objects import AbbreviatedTweet
-from typing import List
+from hanzi_helpers import next_hanzi_summary_tweet, HanziTweetSummary
 
 
 def generate_hanzi_tweets_csv(tweets_js_path: str, csv_output_path: str):
-    summary_tweets = [ {"Date": tweet.created_at.strftime("%Y-%m-%d"),
-                        "Id": tweet.id_int, "Source": tweet.user_mentions[0].name,
-                        "Tweet": tweet.full_text.replace('\n', '')} for tweet in next_hanzi_tweet(tweets_js_path)]
-
+    summary_tweets = [tweet.to_dict() for tweet in next_hanzi_summary_tweet(tweets_js_path)]
     summary_tweets.sort(key=lambda tw: tw["Date"], reverse=True)
 
-    with open(csv_output_path, "w") as csv_out:
-        csv_wrtr = csv.DictWriter(csv_out, fieldnames=("Date", "Id", "Source", "Tweet"), quoting=csv.QUOTE_ALL)
+    with open(csv_output_path, "w", newline='', encoding='utf-8') as csv_out:
+        csv_wrtr = csv.DictWriter(csv_out, fieldnames=HanziTweetSummary.field_names(), quoting=csv.QUOTE_ALL)
         csv_wrtr.writeheader()
         for tw in summary_tweets:
             csv_wrtr.writerow(tw)
@@ -24,5 +18,6 @@ def generate_hanzi_tweets_csv(tweets_js_path: str, csv_output_path: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=pathlib.PurePath(__file__).name, description="Generate markdown files of Chinese=text tweets")
     parser.add_argument('-tweet-js-path', type=str, required=True, help='twitter\'s tweet.js filepath')
+    parser.add_argument('-summary-csv-path', type=str, required=True, help='summary tweets output csv path')
     args = parser.parse_args()
-    generate_hanzi_tweets_csv(args.tweet_js_path, "csv/summary_tweets_chinese_text.csv")
+    generate_hanzi_tweets_csv(args.tweet_js_path, args.summary_csv_path)
