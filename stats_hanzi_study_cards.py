28d58edfdeafd0721c94043e98f046206530f6d1
artxgj <beijingbayou@gmail.com>
2021-12-20 05:33:12 -0800

    added stats_hanzi_study_cards.py and words_tweets_stats.md

---
+++ stats_hanzi_study_cards.py
@@ -0,0 +1,34 @@
+import argparse
+import pathlib
+from hanzi_helpers import next_vocab_tweets_index
+from simple_markdown import h1, table_row, table_header, link, MdCellAlign
+from typing import List
+
+
+def generate_vocab_tweets_stats(*, vocab_tweets_index_path: str,
+                                cards_study_folder: str):
+    vocab_tweets = [(row['Word'], len(row['Tweet_Ids'].split(',')))
+                    for row in next_vocab_tweets_index(vocab_tweets_index_path)]
+    vocab_tweets.sort(key=lambda data: data[1], reverse=True)
+    fields = ['Word/Name', 'Number of Tweets']
+    fields_align = [MdCellAlign.center, MdCellAlign.center]
+    with open(f"{cards_study_folder}/words_tweets_stats.md", "w", encoding='utf-8') as f_out:
+        md_static_part = f"""{h1('Study Words/Names Statistics')}
+        
+{table_header(fields, fields_align)}
+"""
+        f_out.writelines(md_static_part)
+        for word, num_tweets in vocab_tweets:
+            row = [link(word, f"{word}.md"), str(num_tweets)]
+            f_out.writelines(f"{table_row(row)}\n")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=pathlib.PurePath(__file__).name,
+                                     description="generate vocabulary study cards")
+    parser.add_argument('-vocab-tweets-index-path', type=str, required=True, help='words path file')
+    parser.add_argument('-cards-study-path', type=str, required=True, help='cards folder')
+
+    args = parser.parse_args()
+    generate_vocab_tweets_stats(vocab_tweets_index_path=args.vocab_tweets_index_path,
+                                cards_study_folder=args.cards_study_path)
