db4294ed2fe0bd54cd39a2723d0f884b77d3d448
artxgj <beijingbayou@gmail.com>
2021-12-21 03:19:31 -0800

    new hanzi study cards

--- stats_hanzi_study_cards.py
+++ stats_hanzi_study_cards.py
@@ -2,7 +2,6 @@ import argparse
 import pathlib
 from hanzi_helpers import next_vocab_tweets_index
 from simple_markdown import h1, table_row, table_header, link, MdCellAlign
-from typing import List
 
 
 def generate_vocab_tweets_stats(*, vocab_tweets_index_path: str,
@@ -10,17 +9,19 @@ def generate_vocab_tweets_stats(*, vocab_tweets_index_path: str,
     vocab_tweets = [(row['Word'], len(row['Tweet_Ids'].split(',')))
                     for row in next_vocab_tweets_index(vocab_tweets_index_path)]
     vocab_tweets.sort(key=lambda data: data[1], reverse=True)
-    fields = ['Word/Name', 'Number of Tweets']
-    fields_align = [MdCellAlign.center, MdCellAlign.center]
+    fields = ['Rank', 'Word/Name', 'Number of Tweets']
+    fields_align = [MdCellAlign.left, MdCellAlign.center, MdCellAlign.center]
     with open(f"{cards_study_folder}/words_tweets_stats.md", "w", encoding='utf-8') as f_out:
         md_static_part = f"""{h1('Study Words/Names Statistics')}
         
 {table_header(fields, fields_align)}
 """
         f_out.writelines(md_static_part)
+        rank = 1
         for word, num_tweets in vocab_tweets:
-            row = [link(word, f"{word}.md"), str(num_tweets)]
+            row = [str(rank), link(word, f"{word}.md"), str(num_tweets)]
             f_out.writelines(f"{table_row(row)}\n")
+            rank += 1
 
 
 if __name__ == '__main__':
