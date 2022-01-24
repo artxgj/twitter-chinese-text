f9f6b916c80eebeb1fae52f6bdf4301092e9ed47
artxgj <beijingbayou@gmail.com>
2022-01-24 03:33:52 -0800

    generate topics-words-study-2022-JanFeb.md

---
+++ cards_hanzi_words/topics-words-study-2022-JanFeb.md
@@ -0,0 +1,14 @@
+# Study Tweets Containing Topics/Words
+        
+| Word/Name | Number of Tweets | Meaning |
+| ----- | ----- | :---: |
+| [通胀](通胀.md) | 102 | inflation |
+| [爆发](爆发.md) | 76 | erupt; burst out; break out |
+| [短缺](短缺.md) | 166 | shortage |
+| [原油](原油.md) | 29 | crude oil |
+| [苏伊士运河](苏伊士运河.md) | 45 | Suez Canal |
+| [汤加](汤加.md) | 24 | Tonga |
+| [哈萨克斯坦](哈萨克斯坦.md) | 20 | Kazakstan |
+| [农历新年](农历新年.md) | 15 | Chinese/Lunar New Year |
+| [流媒体](流媒体.md) | 21 | streaming media |
+| [纯电动汽车](纯电动汽车.md) | 82 | pure electric vehicle |
---
+++ src/topics_words_study.py
@@ -0,0 +1,50 @@
+import argparse
+import pathlib
+from typing import Generator
+from collections.abc import Mapping
+from src.hanzi_helpers import next_vocab_tweets_index, dictlines_from_csv
+from simple_markdown import h1, table_row, table_header, link, MdCellAlign
+
+
+def generate_companies_tweets_stats(*, vocab_tweets_index_path: str,
+                                    topics_words_csv_path: str,
+                                    topics_words_md_path: str):
+    topics_words = []
+    topics_words_index = {}
+
+    for i, r in enumerate(dictlines_from_csv(topics_words_csv_path)):
+        topics_words.append(r)
+        topics_words_index[r['Word']] = i
+
+    for row in next_vocab_tweets_index(vocab_tweets_index_path):
+        if row['Word'] in topics_words_index:
+            i = topics_words_index[row['Word']]
+            topics_words[i]['tweets'] = len(row['Tweet_Ids'].split(','))
+
+    fields = ['Word/Name', 'Number of Tweets', 'Meaning']
+    fields_align = [MdCellAlign.left, MdCellAlign.left, MdCellAlign.center]
+    with open(topics_words_md_path, "w", encoding='utf-8') as f_out:
+        md_static_part = f"""{h1('Study Tweets Containing Topics/Words')}
+        
+{table_header(fields, fields_align)}
+"""
+        f_out.writelines(md_static_part)
+        rank = 1
+        for word in topics_words:
+            hanzi = word['Word']
+            row = [link(hanzi, f"{hanzi}.md"), str(word['tweets']), word['meaning']]
+            f_out.writelines(f"{table_row(row)}\n")
+            rank += 1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=pathlib.PurePath(__file__).name,
+                                     description="generate topics/words study list.")
+    parser.add_argument('-vocab-tweets-index-path', type=str, required=True, help='words path file')
+    parser.add_argument('-topics-words-csv-path', type=str, required=True, help='topics/words csv path')
+    parser.add_argument('-topics-words-md-path', type=str, required=True, help='topics/words markdown path')
+
+    args = parser.parse_args()
+    generate_companies_tweets_stats(vocab_tweets_index_path=args.vocab_tweets_index_path,
+                                    topics_words_csv_path=args.topics_words_csv_path,
+                                    topics_words_md_path=args.topics_words_md_path)
